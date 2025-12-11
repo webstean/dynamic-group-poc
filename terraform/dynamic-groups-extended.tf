8475b1975a793282e7196b8dc723d5e536ec1b83
Andrew Webster <webstean@gmail.com>
2025-12-12 10:49:12 +1100

    Fix output reference for company name in Azure AD users

--- terraform/dynamic-groups-extended.tf
+++ terraform/dynamic-groups-extended.tf
@@ -15,7 +15,7 @@ output azuread_users_all_enabled {
 
 # Output if required
 output azuread_users_company_name {
-  value = data.azuread_users.all[*].user.company_name 
+  value = data.azuread_users.all[*].users.company_name 
 }
 
 locals {
