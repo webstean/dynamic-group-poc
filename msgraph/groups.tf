d824b7101009272bc1c4d768829328e17efca8e8
Andrew Webster <webstean@gmail.com>
2025-12-12 15:59:10 +1100

    Update groups.tf

--- msgraph/groups.tf
+++ msgraph/groups.tf
@@ -3,7 +3,7 @@ resource "msgraph_resource" "group" {
   body = {
     displayName     = "My Test Group"
     mailEnabled     = false
-    mailNickname    = "mygroupxy"
+    mailNickname    = "mygroup"
     securityEnabled = true
   }
 }
