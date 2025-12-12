2dcf3aa7938be3e984e5e4b2fb703fd5895bedf5
Andrew Webster <webstean@gmail.com>
2025-12-12 14:54:37 +1100

    Update users.tf

--- msgraph/users.tf
+++ msgraph/users.tf
@@ -64,7 +64,7 @@ locals {
   unique_extension_attribute7 = sort(distinct(local.extension_attribute7_clean))
 
   # 6. Turn the list into a set for for_each
-  unique_commpany_set = sort(distinct(local.unique_company))
+  unique_company_set = sort(distinct(local.unique_company))
   unique_extension_attribute5_set = toset(local.unique_extension_attribute5)
   unique_extension_attribute6_set = toset(local.unique_extension_attribute6)
   unique_extension_attribute7_set = toset(local.unique_extension_attribute7)
