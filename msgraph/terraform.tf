7f8c68a9f8c9b13bcb622c4cb95327ce89ba91d5
Andrew Webster <webstean@gmail.com>
2025-12-12 23:08:09 +1100

    Update terraform.tf

--- msgraph/terraform.tf
+++ msgraph/terraform.tf
@@ -7,11 +7,6 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~>4.0, < 5.0"
     }
-#    azuread = {
-#      ## Azure AD (Entra ID)
-#      source  = "hashicorp/azuread"
-#      version = "~>3.0, < 4.0"
-#    }
     msgraph = {
       ## Microsoft Graph - replacement for azuread *future*
       source  = "Microsoft/msgraph"
@@ -35,14 +30,6 @@ provider "azurerm" {
   use_oidc = true
   use_cli  = true
 }
-
-#provider "azuread" {
-#  use_aks_workload_identity = false
-#  use_msi                   = false
-#  # Authentication strategy: Using OIDC (GitHub Actions) and Azure CLI (interactive) for authentication to Azure AD tenant.
-#  use_oidc = true
-#  use_cli  = true
-#}
 
 provider "msgraph" {
   use_aks_workload_identity = false
