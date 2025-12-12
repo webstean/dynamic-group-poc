0f29c14b4dc1f0dea7cc52be5b223101a62ffa38
Andrew Webster <webstean@gmail.com>
2025-12-12 11:59:19 +1100

    Add outputs for display_name and all users

--- msgraph/users.tf
+++ msgraph/users.tf
@@ -5,3 +5,11 @@ data "msgraph_resource" "users" {
     display_name = "displayName"
   }
 }
+
+output "display_name" {
+  value = data.msgraph_resource.users.output.display_name
+}
+
+output "all" {
+  value = data.msgraph_resource.users.output.all
+}
