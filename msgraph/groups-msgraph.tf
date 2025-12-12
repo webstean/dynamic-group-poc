ba2dbd0228b1cfeace4ee503138014454edcd2d4
Andrew Webster <webstean@gmail.com>
2025-12-12 22:14:56 +1100

    Add dynamic groups for each unique department

--- msgraph/groups-msgraph.tf
+++ msgraph/groups-msgraph.tf
@@ -18,3 +18,23 @@ RULE
   }
 }
 
+resource "msgraph_resource" "department_dynamic_groups" {
+  for_each = local.unique_department_set
+
+  url = "groups"
+  body = {
+    displayName     = "Dyn-Department-${each.value}-Members"
+    description     = "Dynamic group for members with companyName == '${each.value}'"
+    mailEnabled     = false
+    securityEnabled = true
+    "owners@odata.bind" = [
+      "https://graph.microsoft.com/v1.0//servicePrincipals/${data.azurerm_client_config.current.object_id}",
+    ]
+    groupTypes = ["DynamicMembership"]
+    membershipRule = <<RULE
+(user.accountEnabled -eq true) and (user.userType -eq "Member") and (user.department -eq "${each.value}")
+RULE
+    membershipRuleProcessingState = "On"
+  }
+}
+
