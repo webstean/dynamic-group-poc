00f23fce014d0504bc65cbeee08ee506b40c9f75
Andrew Webster <webstean@gmail.com>
2025-12-12 17:27:52 +1100

    Update groups.tf

--- msgraph/groups.tf
+++ msgraph/groups.tf
@@ -11,7 +11,7 @@ resource "msgraph_resource" "group" {
 resource "azuread_group_without_members" "company_dynamic_groups" {
   for_each = local.unique_company_set
 
-  display_name     = "xDyn-Company-${each.value}-Members"
+  display_name     = "Dyn-Company-${each.value}-Members"
   description      = "Dynamic group for members with companyName == '${each.value}'"
   security_enabled = true
   mail_enabled     = false
@@ -29,7 +29,7 @@ resource "azuread_group_without_members" "company_dynamic_groups" {
 resource "azuread_group_without_members" "department_dynamic_groups" {
   for_each = local.unique_department_set
 
-  display_name     = "xDyn-Departmnet-${each.value}-Members"
+  display_name     = "Dyn-Departmnet-${each.value}-Members"
   description      = "Dynamic group for members with department == '${each.value}'"
   security_enabled = true
   mail_enabled     = false
@@ -47,7 +47,7 @@ resource "azuread_group_without_members" "department_dynamic_groups" {
 resource "azuread_group_without_members" "exta5_dynamic_groups" {
   for_each = local.unique_extension_attribute5_set
 
-  display_name     = "xDyn-Group-${each.value}-Members"
+  display_name     = "Dyn-Group-${each.value}-Members"
   description      = "Dynamic group for enable members account with extensionAttribute5 == '${each.value}'"
   security_enabled = true
   mail_enabled     = false
@@ -65,7 +65,7 @@ resource "azuread_group_without_members" "exta5_dynamic_groups" {
 resource "azuread_group_without_members" "exta6_dynamic_groups" {
   for_each = local.unique_extension_attribute6_set
 
-  display_name     = "xDyn-Division-${each.value}-Members"
+  display_name     = "Dyn-Division-${each.value}-Members"
   description      = "Dynamic group for enable members account with extensionAttribute6 == '${each.value}'"
   security_enabled = true
   mail_enabled     = false
@@ -83,7 +83,7 @@ resource "azuread_group_without_members" "exta6_dynamic_groups" {
 resource "azuread_group_without_members" "exta7_dynamic_groups" {
   for_each = local.unique_extension_attribute7_set
 
-  display_name     = "xDyn-Branch-${each.value}-Members"
+  display_name     = "Dyn-Branch-${each.value}-Members"
   description      = "Dynamic group for enable members account with extensionAttribute7 == '${each.value}'"
   security_enabled = true
   mail_enabled     = false
