33e709fe0050dca7c9bdb1700af539834e6c174b
Andrew Webster <webstean@gmail.com>
2025-12-12 13:09:16 +1100

    Update users.tf

--- msgraph/users.tf
+++ msgraph/users.tf
@@ -1,12 +1,14 @@
 data "msgraph_resource" "users" {
-  url = "users"
+  url = "users?$select=displayName,givenName,postalCode,identities,companyName"
   response_export_values = {
     all          = "@"
-    display_name = "displayName"
-    company_name = "companyName"
-    city_name = "city"
+#    display_name = "displayName"
+#    company_name = "companyName"
+#    city_name = "city"
   }
 }
+
+GET https://graph.microsoft.com/v1.0/users/87d349ed-44d7-43e1-9a83-5f2406dee5bd?$select=displayName,givenName,postalCode,identities
 
 output "display_names" {
   description = "All Display Names"
