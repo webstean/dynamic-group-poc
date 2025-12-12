1a568ebe14da96109822b4744b39bd0e0c758a20
Andrew Webster <webstean@gmail.com>
2025-12-12 22:03:03 +1100

    Update groups-msgraph.tf

--- msgraph/groups-msgraph.tf
+++ msgraph/groups-msgraph.tf
@@ -21,10 +21,10 @@ resource "msgraph_resource" "company_dynamic_groups" {
 #    "owners@odata.bind" = [
 #      "https://graph.microsoft.com/v1.0/users/${data.azurerm_client_config.current.object_id}",
 #    ]
-#    groupTypes = ["DynamicMembership"]
-#    membershipRule = <<RULE
-#( (user.accountEnabled -eq true) and (user.userType -eq "Member") and (user.companyName -eq "${each.value}") )
-#RULE
-#    membershipRuleProcessingState = "On"
+    groupTypes = ["DynamicMembership"]
+    membershipRule = <<RULE
+( (user.accountEnabled -eq true) and (user.userType -eq "Member") and (user.companyName -eq "${each.value}") )
+RULE
+    membershipRuleProcessingState = "On"
   }
 }
