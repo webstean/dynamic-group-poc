116baa704df38cf2e09ea0fe3c7b5c92a5209eac
Andrew Webster <webstean@gmail.com>
2025-12-12 13:41:52 +1100

    Update users.tf

--- msgraph/users.tf
+++ msgraph/users.tf
@@ -1,7 +1,7 @@
 data "msgraph_resource" "users" {
   url = "users"
   query_parameters = {
-    "$select" = ["id", "mail", "givenName", "surname", "companyName", "postalCode", "onPremisesExtensionAttributes"]
+    "$select" = ["givenName", "surname", "companyName", "onPremisesExtensionAttributes"]
     "$filter" = [
        "accountEnabled eq true and userType eq 'Member'",
     ]
@@ -12,12 +12,24 @@ data "msgraph_resource" "users" {
   }
 }
 
-## GET https://graph.microsoft.com/v1.0/users/87d349ed-44d7-43e1-9a83-5f2406dee5bd?$select=displayName,givenName,postalCode,identities
-
-#output "display_names" {
-#  description = "All Display Names"
-#  value = data.msgraph_resource.users.output.display_name
-#}
+/*
+output "list_company_name" {
+  description = "List of Company Name"
+  value = data.msgraph_resource.users.all.companyName
+}
+output "list_extended_attribute_5" {
+  description = "List of Extended Attribute #5"
+  value = data.msgraph_resource.users.all.onPremisesExtensionAttributes.extensionAttribute5
+}
+output "list_extended_attribute_6" {
+  description = "List of Extended Attribute #6"
+  value = data.msgraph_resource.users.all.onPremisesExtensionAttributes.extensionAttribute6
+}
+output "list_extended_attribute_7" {
+  description = "List of Extended Attribute #7"
+  value = data.msgraph_resource.users.all.onPremisesExtensionAttributes.extensionAttribute7
+}
+*/
 
 output "all" {
   description = "All Users"
