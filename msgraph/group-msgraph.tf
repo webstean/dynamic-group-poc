5c548c947f1b5f59af363535a6f74348fc545e92
Andrew Webster <webstean@gmail.com>
2025-12-12 21:48:19 +1100

    Update and rename msgraph-group.tf to group-msgraph.tf

--- msgraph/group-msgraph.tf
+++ msgraph/group-msgraph.tf
@@ -10,7 +10,9 @@ resource "msgraph_resource" "company_dynamic_groups" {
     "owners@odata.bind" = [
       "https://graph.microsoft.com/v1.0/users/${data.azurerm_client_config.current.object_id}",
     ]
-    membershipRule = "( (user.accountEnabled -eq true) and (user.userType -eq \"Member\") and (user.companyName -eq \"${each.value}\"))"
+    membershipRule = <<RULE
+( (user.accountEnabled -eq true) and (user.userType -eq "Member") and (user.companyName -eq "${each.value}") )
+RULE
     membershipRuleProcessingState = "On"
   }
 }
