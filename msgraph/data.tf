a90dacf7e0362a039e7d43fa05e2fb1b971567f0
Andrew Webster <webstean@gmail.com>
2025-12-12 23:05:49 +1100

    Clean up data sources in data.tf
    Removed unused data sources for azurerm and azuread.

--- msgraph/data.tf
+++ msgraph/data.tf
@@ -1,7 +1,4 @@
 ##azurerm
 data "azurerm_client_config" "current" {}
-data "azurerm_subscription" "current" {}
 data "azurerm_subscriptions" "available" {}
-## azuread
-data "azuread_client_config" "current" {}
 
