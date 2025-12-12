8110ffdf0297fed658999e8ac8c10cf0b06bb1f8
Andrew Webster <webstean@gmail.com>
2025-12-12 21:50:12 +1100

    Disable dynamic membership rules in groups-msgraph.tf
    Comment out group types and membership rules for dynamic membership.

--- msgraph/groups-msgraph.tf
+++ msgraph/groups-msgraph.tf
@@ -6,14 +6,14 @@ resource "msgraph_resource" "company_dynamic_groups" {
     displayName     = "Dyn-Company-${each.value}-Members"
     mailEnabled     = false
     securityEnabled = true
-    groupTypes = ["DynamicMembership"]
     "owners@odata.bind" = [
       "https://graph.microsoft.com/v1.0/users/${data.azurerm_client_config.current.object_id}",
     ]
-    membershipRule = <<RULE
-( (user.accountEnabled -eq true) and (user.userType -eq "Member") and (user.companyName -eq "${each.value}") )
-RULE
-    membershipRuleProcessingState = "On"
+#    groupTypes = ["DynamicMembership"]
+#    membershipRule = <<RULE
+#( (user.accountEnabled -eq true) and (user.userType -eq "Member") and (user.companyName -eq "${each.value}") )
+#RULE
+#    membershipRuleProcessingState = "On"
   }
 }
 
