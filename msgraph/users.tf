00e652a08b946cac2acf9486c2812632932c0716
Andrew Webster <webstean@gmail.com>
2025-12-12 15:03:20 +1100

    Update comment numbering in users.tf

--- msgraph/users.tf
+++ msgraph/users.tf
@@ -63,7 +63,7 @@ locals {
   unique_extension_attribute6 = sort(distinct(local.extension_attribute6_clean))
   unique_extension_attribute7 = sort(distinct(local.extension_attribute7_clean))
 
-  # 6. Turn the list into a set for for_each
+  # 4. Turn the list into a set for for_each
   unique_company_set = sort(distinct(local.unique_company))
   unique_extension_attribute5_set = toset(local.unique_extension_attribute5)
   unique_extension_attribute6_set = toset(local.unique_extension_attribute6)
